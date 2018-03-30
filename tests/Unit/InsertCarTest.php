152bd2b7f9cafc4ef4a5927b28f40403d8f1442e
adarsh <ars94@njit.edu>
2018-03-29 23:05:55 -0400

    unit test to test inserting a car

---
+++ tests/Unit/InsertCarTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class InsertCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertCar()
+    {
+        $car = new Car();
+        $car->Make = 'Honda';
+        $car->Model = 'Petrol';
+        $car->Year = '2016';
+        $this->assertTrue(true);
+    }
+}
